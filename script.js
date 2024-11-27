081c5fa1471416917453018390d1dc1ede4ce279
Júlia <146898047+juujrosario@users.noreply.github.com>
2024-11-27 18:03:28 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Função para scroll suave entre as seções
+function scrollToSection(sectionId) {
+    document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+// Função para rolar suavemente até uma seção
+
